93e15330ce508324c164242dc6c22dc9389d2cc4
lichenfeng <lichenfeng@rongcloud.cn>
2018-11-20 20:35:27 +0800

    问题: new project 原因: 解决方案: Reviewer: 工单号: X Log: X

---
+++ JenkinsTest/JenkinsTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JenkinsTest
+//
+//  Created by lichenfeng on 2018/11/20.
+//  Copyright © 2018年 lichenfeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ JenkinsTest/JenkinsTest/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  JenkinsTest
+//
+//  Created by lichenfeng on 2018/11/20.
+//  Copyright © 2018年 lichenfeng. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
