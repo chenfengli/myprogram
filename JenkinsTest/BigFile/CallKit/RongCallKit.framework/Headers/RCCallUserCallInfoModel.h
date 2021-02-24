fe1e2bdb96cb3ad3f4b3228ec4b1da160c1d8e07
lichenfeng <lichenfeng@rongcloud.cn>
2021-02-24 16:07:12 +0800

    test

--- JenkinsTest/BigFile/CallKit/RongCallKit.framework/Headers/RCCallUserCallInfoModel.h
+++ JenkinsTest/BigFile/CallKit/RongCallKit.framework/Headers/RCCallUserCallInfoModel.h
@@ -1,33 +1,3 @@
-//
-//  RCCallUserCallInfoModel.h
-//  RongCallKit
-//
-//  Created by RongCloud on 16/3/18.
-//  Copyright © 2016年 RongCloud. All rights reserved.
-//
-
-#import <Foundation/Foundation.h>
-#import <RongCallLib/RongCallLib.h>
-#import <RongIMLib/RongIMLib.h>
-
-/*!
- 用户通话信息的数据模型类
- */
-@interface RCCallUserCallInfoModel : NSObject
-
-/*!
- 用户ID
- */
-@property(nonatomic, strong) NSString *userId;
-
-/*!
- 用户信息
- */
-@property(nonatomic, strong) RCUserInfo *userInfo;
-
-/*!
- 用户的通话状态Profile
- */
-@property(nonatomic, strong) RCCallUserProfile *profile;
-
-@end
+version https://git-lfs.github.com/spec/v1
+oid sha256:e97221fed5b8d54aa054980da0a9bf3375669b8a26fe4a6a8a4cd811662a6842
+size 587
